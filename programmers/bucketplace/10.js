e96d4aab22b9f0a1fad37e5041f458c626e30c50
hykimyong <hykimyong@naver.com>
2023-04-09 17:50:12 +0900

    문제풀이추가

---
+++ programmers/bucketplace/10.js
@@ -0,0 +1,32 @@
+const height1 = [130,140];
+const height2 = [130,140];
+solution(height1,height2);
+function solution(height1,height2){
+    let answer = 0;
+    let temp;
+    for(let i = 0; i< height1.length-1; i++){
+        console.log(height1[i],height1[i+1]);
+        console.log( height2[i], height2[i+1]);
+        //왼쪽이 더크면 왼쪽값은 오름차순이 아니니 무조건 바꿔줘야함
+        if(height1[i] > height1[i+1] || height2[i] > height2[i+1]){
+            answer++;
+            temp = height1[i];
+            height1[i] = height2[i];
+            height2[i]= temp;
+        }else if(height1[i] == height1[i+1] || height2[i] == height2[i+1]){
+            //왼쪽꺼를 먼저 바꿔보고 오름차순이 맞으면 바꿔주고
+            if(height1[i-1] < height2[i] && height2[i] < height1[i+1] ){
+                temp = height1[i];
+                height1[i] = height2[i];
+                height2[i]= temp;
+            }else{
+                temp = height1[i+1];
+                height1[i+1] = height2[i+1];
+                height2[i+1]= temp;
+            }
+            answer++;
+        }
+    }
+    console.log(answer);
+    
+}
