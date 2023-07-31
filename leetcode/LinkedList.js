85a76752eda5d9dbd632883d309745836e710458
hykimyong <hykimyong@naver.com>
2023-07-31 22:27:27 +0900

    초기값생성

---
+++ leetcode/LinkedList.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * 링크드 리스트 만들기
+ * 출처 : https://www.freecodecamp.org/korean/news/implementing-a-linked-list-in-javascript/
+ * 
+ */
+const list = {
+    head: {
+        value: 6,
+        next: {
+            value: 10,                                        
+            next: {
+                value: 12,
+                next: {
+                    value: 3,
+                    next: null	
+                }
+            }
+        }
+    }
+}
