030002d1705ae3fb174e4a1702ef8ff04ba147b1
hykimyong <hykimyong@naver.com>
2023-08-01 19:51:41 +0900

    노드 만들기

--- leetcode/LinkedList.js
+++ leetcode/LinkedList.js
@@ -19,3 +19,22 @@ const list = {
         }
     }
 }
+
+class ListNode {
+    constructor(data) {
+        this.data = data
+        this.next = null                
+    }
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head
+    }
+}
+
+let node1 = new ListNode(2);
+let node2 = new ListNode(5);
+node1.next = node2;
+
+console.log(node2);
