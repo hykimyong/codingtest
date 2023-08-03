a12ab9fc3dadea144d69d50ea2c1eb35976effdd
hykimyong <hykimyong@naver.com>
2023-08-03 22:20:50 +0900

    size추가

--- leetcode/LinkedList.js
+++ leetcode/LinkedList.js
@@ -31,6 +31,19 @@ class LinkedList {
     constructor(head = null) {
         this.head = head
     }
+    size() {
+        let count = 0; 
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next
+        }
+        return count;
+    }
+    clear() {
+        this.head = null;
+    }
+    
 }
 
 let node1 = new ListNode(2);
@@ -38,5 +51,6 @@ let node2 = new ListNode(5);
 node1.next = node2;
 
 let list = new LinkedList(node1);
+
 
 console.log(list.head.next.data);
